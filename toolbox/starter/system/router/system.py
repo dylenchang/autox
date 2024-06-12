f4a9eba0d1b455080043ad0410ba5069f7eda169
dylenchang <511250051@qq.com>
2024-06-12 22:13:46 +0800

    feat: add the data model of the host list.

--- toolbox/starter/system/router/system.py
+++ toolbox/starter/system/router/system.py
@@ -6,12 +6,10 @@ from toolbox.common.config import configs
 from toolbox.starter.server import app
 from toolbox.starter.system.api.v1.probe_controller import probe_router
 from toolbox.starter.system.api.v1.user_controller import user_router
-from toolbox.starter.system.api.v1.role_controller import role_router
 
 system_router = APIRouter()
 system_router.include_router(probe_router, tags=["probe"], prefix="/probe")
 system_router.include_router(user_router, tags=["user"], prefix="/user")
-system_router.include_router(role_router, tags=["role"], prefix="/role")
 
 # Add system routing
 app.include_router(system_router, prefix=configs.api_version)
--- toolbox/starter/system/service/role_service.py
+++
@@ -1,9 +0,0 @@
-"""Role domain service interface"""
-
-from abc import ABC
-
-from toolbox.common.service.service import Service
-from toolbox.starter.system.model.role_do import RoleDO
-
-
-class RoleService(Service[RoleDO], ABC): ...
